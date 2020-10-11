0f875fdf84963e56fcb46a19a4e1d83c8cae897f
Bishesh Sunam <bisheshsunam@Bisheshs-MacBook-Pro.local>
2020-10-12 12:37:40 +1300

    styled header

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SearchBox() {
     return (
         <div className="tc w-50 ml-auto mr-auto mb3 p2 bg-light-green shadow-5 br4">
-            <h1>Robo-Friends</h1>
+            <h1 className="pt3">Robo Friends</h1>
             <input placeholder="search robots" className="tc p2 br3 h2 mb3"></input>
         </div>
     )
