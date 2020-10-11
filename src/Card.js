8d8fd91bcd09bcd0b6080a8da97bcf18d9461f4e
Bishesh Sunam <bisheshsunam@Bisheshs-MacBook-Pro.local>
2020-10-12 04:52:58 +1300

    modified tychyons classess on header

--- src/Card.js
+++ src/Card.js
@@ -8,7 +8,7 @@ function Card() {
             {roborts.map((robort)=>{
                
              return(
-                <div key={robort.id} className="tc bg-light-green dib br3 pa3 ma2 mt10 grow bw2 shadow-5">
+                <div key={robort.id} className="tc bg-light-green dib br4 pa3 ma2 grow bw2 shadow-5">
                     <img src={`https://robohash.org/${robort.id}/200x200`} alt="roborts"></img>
                     <h2>{robort.first_name}</h2>
                     <p>{robort.email}</p>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SearchBox() {
     return (
-        <div className="tc w-50 ml-auto mr-auto mb6 p2 bg-light-green shadow-5 br3">
+        <div className="tc w-50 ml-auto mr-auto mb3 p2 bg-light-green shadow-5 br4">
             <h1>Robo-Friends</h1>
             <input placeholder="search robots" className="tc p2 br3 h2 mb3"></input>
         </div>
