613a5be09e173a2cd7d9e103ecd81604024fcbb0
Bishesh <bishesh.sunam@gmail.com>
2020-10-13 16:49:02 +1300

    Updates

---
+++ precache-manifest.092c22c28cb44d6ac5ad99ae526395ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a5c1e7a56713d644dc3bf36a8a6194",
+    "url": "/robofriend-react-app/index.html"
+  },
+  {
+    "revision": "e2ea2b102517e516e30c",
+    "url": "/robofriend-react-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8e8135bcb49b8a208376",
+    "url": "/robofriend-react-app/static/css/main.f9d19243.chunk.css"
+  },
+  {
+    "revision": "e2ea2b102517e516e30c",
+    "url": "/robofriend-react-app/static/js/2.9deda619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend-react-app/static/js/2.9deda619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e8135bcb49b8a208376",
+    "url": "/robofriend-react-app/static/js/main.0f7d4630.chunk.js"
+  },
+  {
+    "revision": "38b13c45cd2a9c0ee764",
+    "url": "/robofriend-react-app/static/js/runtime-main.b83a01e0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/robofriend-react-app/precache-manifest.092c22c28cb44d6ac5ad99ae526395ec.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriend-react-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
